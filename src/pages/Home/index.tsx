b3fe9cec6b8f7a047b2c6c96409d01423262a18e
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-02-09 23:51:46 -0300

    Commit final

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,6 @@ const Home: React.FC = () => (
           <div>
             <span />
           </div>
-          {/* <img src={imagem.imagem} alt="" /> */}
         </ImagesJob>
       ))}
     </GridImages>
